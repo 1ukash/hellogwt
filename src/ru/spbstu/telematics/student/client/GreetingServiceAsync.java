83ca9ff6999ff3b2e8dc9a161bd2cd1f4fffb1f4
Alexey A. Lukashin <alexey@lukash.spb.ru>
2011-05-03 21:47:47 +0400

    inited

---
+++ src/ru/spbstu/telematics/student/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package ru.spbstu.telematics.student.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+  void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+}
